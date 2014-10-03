a0f857c4d0fe1d03cd239b633aaa2072d8a6bf59
nabbs <nabiarshad@gmail.com>
2014-10-04 00:24:06 +0100

    changes

--- js/site.js
+++ js/site.js
@@ -15,14 +15,29 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        event.preventDefault();		
+				
+				/*$(window).scroll(function() {
+					if($(window).scrollTop() == $(document).height() - $(window).height()) {
+						var current =  $(this).prop("hash");
+						$(current).delay(500).slideDown(300,'linear');
+						console.log(current);
+						return false;
+					}
+				});*/
     });
 });
+
+/*$(function() {
+    $('a.page-scroll').on("click", function() {
+        alert($(document).scrollTop());
+    });
+});*/
+
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
-		//console.log('responisive clicked');
 });
 
 
@@ -36,10 +51,26 @@ $(document).ready(function(){
     targetSlide = parseInt($(this).attr('data-to'));
     $('#myCarousel').carousel(targetSlide);
 		$('#myModalLabel').text($(this).attr('title'));
+		var itemContent = $('.carousel-inner div.active div').html();
+		$('.modal-footer').html(itemContent);
 	});
 	
 	$('#myCarousel').on('slid.bs.carousel', function () {
 		var current = $('.carousel-inner div.active').attr('title');
+		var itemContent = $('.carousel-inner div.active div').html();
 			$('#myModalLabel').text(current);
+			$('.modal-footer').html(itemContent);
 	});
+	
+	$('header h1').delay(500).slideDown(300,'linear');
+	$('header p').delay(800).slideDown(300,'linear');	
 });
+
+/*$('nav a').each(function() {
+	$(this).click(function() {
+		var current =  $(this).prop("hash");
+		$(current).slideDown(300,'linear');  
+		console.log(current);
+		return false;
+	});
+});*/
